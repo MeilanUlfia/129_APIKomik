48b785accee522b60a905d0ffc2d8fc6ea65c53f
meilan <meilanulfia@gmail.com>
2026-07-29 14:50:55 +0700

    membuat controller user

---
+++ controller/userController.js
@@ -0,0 +1,8 @@
+const db = require('../models');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const user = require('../models/user');
+
+const User = db.User;
+
+module.exports = {};
