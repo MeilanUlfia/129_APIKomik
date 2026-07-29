f17bc3b947c52c071249d92a185d556c469e17a9
meilan <meilanulfia@gmail.com>
2026-07-29 14:43:09 +0700

    menambahkan endpoint melihat seluruh data komik

--- controller/komikController.js
+++ controller/komikController.js
@@ -1,3 +1,16 @@
 const db = require('../models');
 
-module.exports = {};
+async function getAllKomik(req, res) {
+    try {
+        const komik = await db.Komik.findAll();
+        res.status(200).json(komik);
+    } catch (err) {
+        console.error('Error fetching komik:', err.message);
+        res.status(500).json({ error: 'Failed to fetch komik' });
+    }
+}
+
+module.exports = {
+    getAllKomik,
+
+};
